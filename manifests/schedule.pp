ec9235caf84797696a6c90150c8a57c13eb2fcea
Chris Andrews <triforce@users.noreply.github.com>
2025-05-14 11:55:44 +0100

    Update schedule.pp

--- manifests/schedule.pp
+++ manifests/schedule.pp
@@ -57,16 +57,16 @@
 #  }
 #
 define yum_autoupdate::schedule (
-  $action       = 'apply',
-  $exclude      = [],
-  $notify_email = true,
-  $email_to     = 'root',
-  $email_from   = 'root',
-  $debug_level  = $yum_autoupdate::params::debug_level,
-  $error_level  = 0,
-  $skip_broken  = false,
-  $update_cmd   = 'default',
-  $randomwait   = 60,
+  Pattern[/^(check|download|apply)$/] $action = 'apply',
+  Array $exclude      = [],
+  Boolean $notify_email = true,
+  String $email_to     = 'root',
+  String $email_from   = 'root',
+  Integer $debug_level  = $yum_autoupdate::params::debug_level,
+  Integer $error_level  = 0,
+  Boolean $skip_broken  = false,
+  Pattern[/^(default|security|security-severity:Critical|minimal|minimal-security|minimal-security-severity:Critical)$/] $update_cmd   = 'default',
+  Integer $randomwait   = 60,
   #cron attributes,
   $user         = root,
   $hour         = undef,
@@ -81,13 +81,8 @@ define yum_autoupdate::schedule (
   }
 
   # parameters validation
-  validate_re($action, '^(check|download|apply)$', '$action must be either \'check\', \'download\' or \'apply\'')
-  validate_array($exclude)
-  validate_bool($notify_email, $skip_broken)
-  validate_string($email_to, $email_from, $update_cmd)
   if ($debug_level < -1) or ($debug_level > 10) { fail('$debug_level must be a number between -1 and 10') }
   if ($error_level < 0) or ($error_level > 10) { fail('$error_level must be a number between 0 and 10') }
-  validate_re($update_cmd, '^(default|security|security-severity:Critical|minimal|minimal-security|minimal-security-severity:Critical)$', '$update_cmd must be either \'default\', \'security\', \'security-severity:Critical\', \'minimal\', \'minimal-security\' or \'minimal-security-severity:Critical\'')
   if ($randomwait < 0) or ($randomwait > 1440) { fail('$randomwait must be a number between 0 and 1440') }
 
   # set real debug level
